eca190bf92fd261459807cd627c1377e814afcab
iOS <iOS@kareems-Mac.local>
2017-12-11 22:20:52 +0200

    Initial Commit

---
+++ firstapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  firstapp
+//
+//  Created by iOS on 12/11/17.
+//  Copyright © 2017 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("ASDASD")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
